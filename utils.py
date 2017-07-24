3b075dff73f2c9e9c6fbca831e00e0778dc4e1ee
Ayush Saraf <ayush29f@uw.edu>
2017-07-23 21:29:35 -0700

    initial version of data layer done

--- utils.py
+++ utils.py
@@ -76,6 +76,7 @@ def reshape_to_square(img, size=256):
 				in a size x size square.
 	"""
 	# TODO: make sure this formula is correct
+	# TODO: draw in a square instead of rescaling
 	img_resize = imresize(img, float(size) / max(img.shape))
 	w_, h_ = img_resize.shape
 	x, y = ((size - w_) / 2, (size - h_) / 2)
@@ -89,22 +90,43 @@ class DataLoader(object):
 	"""Class for loading data."""
 
 	def __init__(self,
+				dataset_type='train',
 				batch_size=25,
 				img_size=256,
-				dataset_base_url=QUICK_DRAW_BASE_URL,
-				categories_file=QUICK_DRAW_CATEGORIES_URL,
+				count_per_category=2000,
 				data_format='bitmap',
 				datastore_dir='/tmp/quick-draw-net/data/',
+				dataset_base_url=QUICK_DRAW_BASE_URL,
+				categories_file=QUICK_DRAW_CATEGORIES_URL,
 				shuffle=False):
+		assert batch_size < count_per_category
+		assert count_per_category % batch_size == 0
+
+		self.dataset_type = dataset_type
 		self.batch_size = batch_size
 		self.img_size = img_size
+		self.count_per_category = count_per_category
 		self.data_format = data_format
 		self.datastore_dir = datastore_dir
 		self.dataset_base_url = dataset_base_url
+		
+		self.categories_path = {}
+		self.categories_url = {}
+		self.idx_to_categories = self.load_categories(categories_file)
 
-		self.load_categories_url(categories_file)
+		self.idx_category = 0
+		self.idx = 0
 
-	def load_categories_url(self, categories_file):
+		self.strokes = {}
+		category = self.idx_to_categories[self.idx_category]
+		if category not in self.strokes:
+			self.load_strokes(category)
+	
+		# TODO: Store bitmaps so that we don't have to recompute bitmaps
+		# if self.data_format == 'bitmap':
+		# 	self.bitmaps = {}
+		
+	def load_categories(self, categories_file):
 		"""Loads the categories line seperated file with all the categories to
 		loaded by the DataLoader. Adds the path to the dataset file in the
 		categories_path dictionary.
@@ -112,8 +134,6 @@ class DataLoader(object):
 		Args:
 			categories_file: path to the line seperated file with desired category names
 		"""
-		self.categories_path = {}
-		self.categories_url = {}
 		categories = []
 		if categories_file.startswith('http://') or categories_file.startswith('https://'):
 			response = requests.get(categories_file)
@@ -129,9 +149,9 @@ class DataLoader(object):
 		# add the urls if they are valid and give status 200
 
 		for category in categories:
-			category_url = os.path.join(self.datastore_dir, '%s.npz' % category)
-			if os.path.isfile(category_url):
-				self.categories_path[category] = category_url
+			category_path = os.path.join(self.datastore_dir, '%s.npz' % category)
+			if os.path.isfile(category_path):
+				self.categories_path[category] = category_path
 
 			category_url = os.path.join(self.dataset_base_url, '%s.npz' % category)
 			response = requests.head(category_url)
@@ -140,9 +160,9 @@ class DataLoader(object):
 			else:
 				logging.warn('Category %s was not found' % category)
 
-			category_full_url = os.path.join(self.datastore_dir, '%s.full.npz' % category)
-			if os.path.isfile(category_full_url):
-				self.categories_path[category] = category_full_url
+			category_full_path = os.path.join(self.datastore_dir, '%s.full.npz' % category)
+			if os.path.isfile(category_full_path):
+				self.categories_path['%s.full' % category] = category_full_path
 
 			category_full_url = os.path.join(self.dataset_base_url, '%s.full.npz' % category)
 			response = requests.head(category_full_url)
@@ -151,11 +171,14 @@ class DataLoader(object):
 			else:
 				logging.warn('Category %s (full) was not found' % category)
 
-	def download_dataset(self, category, full=False):
-		"""Downloads the dataset for the requested category to the data_dir
+		return categories
+
+	def load_strokes(self, category, full=False):
+		"""Loads the strokes for the requested category. It Downloads the 
+		dataset to the datastore_dir if not already available.
 
 		Args:
-			category: category of the dataset to be downloaded
+			category: category of the dataset to be loaded
 			full: a boolean flag to download the complete dataset file for
 					the given category
 		"""
@@ -172,6 +195,7 @@ class DataLoader(object):
 						% (self.categories_url[category_], response.status_code))
 
 			with open(os.path.join(self.datastore_dir, '%s.npz' % category_), 'w') as handle:
+				# TODO: log downloading progress
 				for block in response.iter_content(1024):
 					handle.write(block)
 
@@ -180,7 +204,36 @@ class DataLoader(object):
 		# load the category data into memory
 		strokes = np.load(StringIO(open(self.categories_path[category_]).read()))
 
-		return strokes['train']
+		# TODO: look for optimization, loads all then returns count_per_category items
+		self.strokes[category_] = strokes[self.dataset_type][:self.count_per_category]
+
+	def _increment_idx(self, step_size):
+		self.idx += step_size
+		if self.idx >= self.count_per_category:
+			self.idx_category += 1
+			self.idx_category %= len(self.idx_to_categories)
+			category = self.idx_to_categories[self.idx_category]
+			if category not in self.strokes:
+				self.load_strokes(category)
+			self.idx %= self.count_per_category
 
 	def next_batch(self):
-		pass
+		category = self.idx_to_categories[self.idx_category]
+		idx_end = min(self.idx+self.batch_size, self.count_per_category)
+		strokes_batch = self.strokes[category][self.idx:idx_end]
+
+		self._increment_idx(self.batch_size)
+		
+		if len(strokes_batch) < self.count_per_category:
+			category = self.idx_to_categories[self.idx_category]
+			np.append(strokes_batch, self.strokes[category][0:self.idx])
+
+		if self.data_format != 'bitmap':
+			return strokes_batch
+
+		# convert to strokes to bitmap and return
+		strokes_to_sqnp = lambda x: reshape_to_square(strokes_to_npy(x))
+		bitmaps_batch = np.array(map(strokes_to_sqnp, strokes_batch))
+		# TODO: Store bitmaps so that we don't have to recompute bitmaps
+		return bitmaps_batch
+		
